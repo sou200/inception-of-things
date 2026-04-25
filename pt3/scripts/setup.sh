3f3585b438414274f87fcab6579f823e1c745b41
Soufiane Erhouni <serhouni@e1r4p13.1337.ma>
2026-04-25 22:22:06 +0100

    add install script

--- pt3/scripts/setup.sh
+++ pt3/scripts/setup.sh
@@ -1,12 +1,8 @@
-apt-get update
-apt-get install -y curl
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.0.0 bash
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-curl -fsSL https://get.docker.com | sh
-
-sudo usermod -aG docker vagrant
-k3d cluster create mycluster --servers 1 --agents 1
+# k3d cluster create mycluster --servers 1 --agents 1
+k3d cluster create mycluster --servers 1 --agents 1 \
+  -p "80:80@loadbalancer" \
+  -p "443:443@loadbalancer"
 kubectl create namespace argocd
 kubectl create namespace dev
 
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
