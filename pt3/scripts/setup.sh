78b63ab95739e74feb3086650c17c673bd1a5c43
Soufiane Erhouni <serhouni@e1r5p7.1337.ma>
2026-04-26 18:12:56 +0100

    add delete script

--- pt3/scripts/setup.sh
+++ pt3/scripts/setup.sh
@@ -1,8 +1,7 @@
 # k3d cluster create mycluster --servers 1 --agents 1
-k3d cluster create mycluster --servers 1 --agents 1 \
-  -p "80:80@loadbalancer" \
-  -p "443:443@loadbalancer"
+k3d cluster create mycluster --servers 1 --agents 1
 kubectl create namespace argocd
 kubectl create namespace dev
 
 kubectl create -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sudo kubectl -n argocd port-forward --address 0.0.0.0 svc/argocd-server 8080:80 &
