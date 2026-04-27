8932bba69e03752eb515a5abdace49967ef3197d
sou200 <sfh2000far@gmail.com>
2026-04-27 16:10:58 +0000

    k

--- pt3/scripts/setup.sh
+++ pt3/scripts/setup.sh
@@ -1,5 +1,5 @@
 k3d cluster delete mycluster
-k3d cluster create mycluster --servers 1 --agents 1
+k3d cluster create mycluster --servers 1 --agents 1 -p "80:80@loadbalancer" -p "443:443@loadbalancer"
 kubectl create namespace argocd
 kubectl create namespace dev
 
@@ -7,7 +7,6 @@ sleep 5
 
 kubectl create -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
-#nohup sudo kubectl -n argocd port-forward --address 0.0.0.0 svc/argocd-server 8080:80 &
 kubectl apply -f ../configs/
 
 sleep 5
