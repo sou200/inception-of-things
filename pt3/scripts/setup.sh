e09009a904c33a8460cdf975ab26dc54ba41a9d0
Soufiane Erhouni <serhouni@e1r5p7.1337.ma>
2026-04-27 13:31:58 +0100

    modify ingress file

--- pt3/scripts/setup.sh
+++ pt3/scripts/setup.sh
@@ -3,10 +3,10 @@ k3d cluster create mycluster --servers 1 --agents 1
 kubectl create namespace argocd
 kubectl create namespace dev
 
+sleep 5
+
 kubectl create -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
 kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
-
 #nohup sudo kubectl -n argocd port-forward --address 0.0.0.0 svc/argocd-server 8080:80 &
 kubectl apply -f ../configs/
 
