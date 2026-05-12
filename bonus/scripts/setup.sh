6bd600b2855267448e1186bae50b6e071883137c
Soufiane Erhouni <serhouni@e1r5p10.1337.ma>
2026-05-12 18:21:04 +0100

    clean project

--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -8,18 +8,25 @@ kubectl create namespace argocd
 kubectl create namespace dev
 kubectl create namespace gitlab
 
+#install nginx
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+
+#install argocd
+kubectl apply -n argocd --server-side -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+#install gitlab
 helm install gitlab gitlab/gitlab \
   -n gitlab \
   -f ../configs/values.yaml
 
-sleep 5
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -n argocd --server-side -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
-kubectl apply -f ../configs/
+kubectl wait --for=condition=available --timeout=600s deployment/gitlab-webservice-default -n gitlab
+
+cd ../configs
+kubectl apply -f secret.yaml -f Application.yaml -f ingress.yaml
 
 argocd_password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-gitlab_password=$(kubectl get secret gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" -n gitlab | base64 -d)
+gitlab_password=$(kubectl -n gitlab get secret gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" | base64 -d)
 
 echo "Argo CD initial admin password: $argocd_password"
 echo "gitlab initial root password: $gitlab_password"
